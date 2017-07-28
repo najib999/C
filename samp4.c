52e2b5c4083b1f9e8ff4946f25b1b73cde1a1eba
Shariar Kabir <najib999@gmail.com>
2017-07-27 23:26:56 -0500

    Create samp4.c

---
+++ samp4.c
@@ -0,0 +1,16 @@
+typedef struct {
+    int x;
+    int y;
+}Cor;
+
+void print(const Cor *a){
+    printf("(x,y) = <%d,%d>\n", a->x, a->y);
+}
+
+int main()
+{
+    Cor p = {2, 3};
+    printf("(x,y) = <%d,%d>\n", p.x, p.y);
+    print(&p);
+    return 0;
+}
